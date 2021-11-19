9efe32fac4acff1ae6e3f4b2aa8da145d15e4f92
trunghieule0301 <63578252+trunghieule0301@users.noreply.github.com>
2021-11-19 07:20:25 +0700

    Delete src directory

--- src/service/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const HTTP = axios.create('https://dev-online-gateway.ghn.vn/shiip/public-api/')
-axios.create({
-    baseURL: 'https://dev-online-gateway.ghn.vn/shiip/public-api/'
-});
-
-export default HTTP
